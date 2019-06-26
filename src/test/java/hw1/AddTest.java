50d6759a3b29997843bb1615ce8b42422c9d25a0
Khodakovsky, Dmitry <Dmitrii.Khodakovsky@Clarivate.com>
2019-06-26 19:26:30 +0300

    HW1 & HW2 is accepted

--- src/test/java/hw1/AddTest.java
+++ src/test/java/hw1/AddTest.java
@@ -6,12 +6,6 @@ import static org.testng.Assert.assertEquals;
 
 public class AddTest extends BaseClass {
 
-    // TODO It could be extracted to BaseClass - fixed
-
-    // TODO It could be extracted to BaseClass - fixed
-    // TODO Why is it BeforeTest? - changed to BeforeMethod in new class BaseClass
-    // I read about the difference between the annotations once again
-
     @Test(dataProviderClass = DataProviders.class,
             dataProvider = "add-data-set-double")
     public void addTestDouble(double a, double b, double result) {
